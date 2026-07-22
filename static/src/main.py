fa009c02281eee1ad10a61f838584806d0c81538
Andreas Flodhag <aflodhag@gmail.com>
2026-07-22 10:10:17 +0200

    2 - Backing up

---
+++ static/src/main.py
@@ -0,0 +1,11 @@
+from textnode import TextNodeType
+
+print("hello world")
+
+
+def main():
+    message = TextNodeType("Some text,", "Some more text", "https://www.boot.dev")
+    print(message)
+
+
+main()
